8bdb7c58de1daa21490cfa69fd67a9650b53c445
rajna <rajnareju@gmail.com>
2023-10-23 22:10:54 +0530

    fontsizechanged

--- App.js
+++ App.js
@@ -1,100 +1,177 @@
-import { StyleSheet, Text, View,Image,TouchableOpacity,}from 'react-native'
-import { useState } from 'react'
-import { isEnabled } from 'react-native/Libraries/Performance/Systrace';
- const App=() => {
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import { isEnabled } from "react-native/Libraries/Performance/Systrace";
+const App = () => {
+  const [enable, setenable] = useState(false);
 
- const[enable,setenable,]=useState(false)
- 
- const onclickMe=() => {
- console.log(enable,'clicked');
-  setenable(!enable);
-}
+  const onclickMe = () => {
+    console.log(enable, "clicked");
+    setenable(!enable);
+  };
   return (
-    <View style={{
-      flex:1,
-      backgroundColor:enable? 'orange':'grey',
-    }}>
-    <Text style={{
-      fontSize:35,
-      color:'white',
-      marginTop:36,
-      fontWeight:'bold',
-      textAlign:'center'
-    }}>Bedroom</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: enable ? "orange" : "grey",
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
 
-    <view style={{
-      height:2,
-      width:530,
-      marginTop:2,
-      backgroundColor:'white'
-    }}></view>
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
+          <view
+            style={{
+              flex: 1,
+              height: 2,
 
-    <view style={{
-      height:2,
-      width:520,
-      marginTop:2,
-      marginLeft:700,
-      backgroundColor:'white'
-    }}></view>
+              backgroundColor: "white",
+            }}
+          ></view>
+          <Text
+            style={{
+              fontSize: 35,
+              color: "white",
 
-    <Text style={{
-      fontSize:36,
-      color:'white',
-      marginBottom:6,
-      fontWeight:'bold',
-      textAlign:'center'
-    }}>Lights</Text>
+              fontWeight: "bold",
+              textAlign: "center",
+            }}
+          >
+            Bedroom{"\n"}Lights
+          </Text>
+          <view
+            style={{
+              flex: 1,
 
-    <Image style={{
-      height:180,
-      width:200,
-      marginTop:65,
-      alignSelf:'center'
-      }} 
-      resizeMode="contain"
-     source={enable ? require('./assets/30535-2-idea-bulb-clipart-thumb.png'):require('./assets/13444.png')}/>
+              height: 2,
 
-    <TouchableOpacity 
-     onPress={
-      ()=> {
-        onclickMe();
-      }
-    }
-    style={{
-      height:50,
-      width:130,
-      borderRadius:55,
-      marginLeft:480,
-      marginTop:100,
-      backgroundColor:enable?'yellow':'black',
-    
-     }}>
-    <Text style={{
-      fontSize:25,
-      color:'white',
-      alignItems:'center',
-      textAlign:'left',
-      marginLeft:4,
-      marginTop:4,
-    }}>
-    {enable ? 'ON' : 'OFF'} 
-    </Text>
-    <View style={{
-      height:48,
-      width:48,
-      borderRadius:48/2,
-      backgroundColor:'white',
-      position:'absolute',
-      marginBottom:4,
-      marginLeft:81
-    }}></View>
-    
-    </TouchableOpacity>
+              backgroundColor: "white",
+            }}
+          ></view>
+        </View>
+      </View>
+      <View
+        style={{
+          flex: 2,
+        }}
+      >
+        <Image
+          style={{
+            height: "100%",
+            width: "100%",
 
+            alignSelf: "center",
+          }}
+          resizeMode="contain"
+          source={
+            enable
+              ? require("./assets/30535-2-idea-bulb-clipart-thumb.png")
+              : require("./assets/13444.png")
+          }
+        />
+      </View>
+
+      <View
+        style={{
+          flex: 1,
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => {
+            onclickMe();
+          }}
+          style={{
+            height: 60,
+            width: 150,
+            borderRadius: 50,
+            alignSelf: "center",
+            marginTop: 100,
+            backgroundColor: enable ? "yellow" : "black",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          {enable === false && (
+            <View
+              style={{
+                height: 50,
+                width: 50,
+                borderRadius: 50 / 2,
+                backgroundColor: "white",
+                marginLeft: 5,
+              }}
+            ></View>
+          )}
+
+          {enable === true && (
+            <View
+              style={{
+                justifyContent: "center",
+                alignItems: "center",
+                marginLeft: 28,
+               
+              }}
+            >
+              <Text
+                style={{
+                  color: "black",
+                  fontSize: 18,
+                  fontWeight: "bold",
+                }}
+              >
+                ON
+              </Text>
+            </View>
+          )}
+          {!enable && (
+            <View
+              style={{
+                
+                justifyContent: "center",
+                alignItems: "center",
+                marginLeft: 12,
+                
+               
+              }}
+            >
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: 18,
+                  fontWeight: "bold",
+                }}
+              >
+                OFF
+              </Text>
+            </View>
+          )}
+
+          <View
+            style={{
+              height: 50,
+              width: 50,
+              borderRadius: 50 / 2,
+              backgroundColor: "black",
+              marginRight: 5,
+            }}
+          ></View>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
